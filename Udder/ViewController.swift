12fef8a7ad1cabacd32512c0ef316253269d89e8
user132928 <user132928@eu245.local>
2017-12-10 19:07:15 +0000

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by user132928 on 12/10/17.
+//  Copyright © 2017 user132928. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ UdderTests/UdderTests.swift
@@ -0,0 +1,36 @@
+//
+//  UdderTests.swift
+//  UdderTests
+//
+//  Created by user132928 on 12/10/17.
+//  Copyright © 2017 user132928. All rights reserved.
+//
+
+import XCTest
+@testable import Udder
+
+class UdderTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
